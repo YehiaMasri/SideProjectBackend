dcdc5d2fcc23be954ab70bd58744d39e3eae6a32
YahyaMasri <yehiamasri83@gmail.com>
2023-04-04 10:46:05 +0300

    setup backend

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from "dotenv"
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = new express();
+
+if (process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`))
